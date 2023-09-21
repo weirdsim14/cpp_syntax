2ee37cf9cab3672e04b81a7aecfa09f8946b12fa
weirdsim14 <sim30217@gmail.com>
2023-09-22 01:53:19 +0900

    first

---
+++ storage_class.cpp
@@ -0,0 +1,13 @@
+void show(void);
+int x; //global variable
+//storing at data segment
+int main(void)
+{
+    int y; // local variable
+    // storing at stack
+}
+
+void show(void)
+{
+    int z; // local variable
+}
